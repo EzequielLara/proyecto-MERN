96f25b46c78912d6845daa51ecb4e18a52be270b
Ezequiel <ezekdaw@gmail.com>
2021-07-31 19:41:05 +0200

    Reducer

---
+++ src/context/proyectos/ProyectoState.js
@@ -0,0 +1,28 @@
+import proyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer';
+import { useReducer } from 'react';
+
+const ProyectoState = props =>{
+
+        const initialState = {
+            formulario : false
+        }
+
+        //Dispach para ejecutar las acciones
+        const [state, dispatch] = useReducer(proyectoReducer, initialState);
+
+        //Serie de funciones para el CRUD
+
+        return (
+            <proyectoContext.Provider
+                value ={{
+                    formulario: state.formulario
+                }}
+            >
+                {props.children}
+        </proyectoContext.Provider>
+        )
+
+    }
+
+    export default ProyectoState;
---
+++ src/context/proyectos/proyectoContext.js
@@ -0,0 +1,5 @@
+import React, {createContext, useState} from 'react';
+
+ const proyectoContext = createContext();
+
+ export default proyectoContext;
