2c7902f43209fa2b1e8b6823911463d0ead415da
Ezequiel <ezekdaw@gmail.com>
2021-08-02 17:07:21 +0200

    mostrando nombre de la tarea

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import Tarea from './Tarea'
+import React, {useContext} from 'react';
+import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext'
 
 const ListadoTareas = () => {
+    
+    //obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+    //Si no hya proyecto seleccionado
+    if(!proyecto){ return <h2>Selecciona un proyecto</h2>;}
 
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
+    
     const tareas = [
         {nombre: 'Elegir Plataforma', estado: true},
         {nombre: 'Elegir Plataforma', estado: true},
@@ -13,7 +23,7 @@ const ListadoTareas = () => {
 
     return (
       <>
-        <h2>Proyecto: Tienda Virtual</h2>
+        <h2>Proyecto: {proyectoActual.nombre}</h2>
 
         <ul className = 'listado-tareas'>
             {tareas.length === 0
